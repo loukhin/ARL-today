f5a6a8ee0f58b7b197c884b18e955b4450e2b009
LoukHin <hatsuwara3@gmail.com>
2019-10-23 09:02:46 +0000

    Updated DB structure

--- db_restore.sh
+++ db_restore.sh
@@ -1,2 +1,2 @@
-echo "Restoring data from mongodb/dump/"
-docker-compose exec db mongorestore /data/db/dump/
+echo "Restoring data from mongodb/dump/arl"
+docker-compose exec db mongorestore --uri="mongodb://arl:ARL3141@db:27017/arl" /data/db/dump/
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 echo "Backing up Database to mongodb/dump/"
-docker-compose exec db mongodump -o /data/db/dump/
+docker-compose exec db mongodump -o /data/db/dump/ --uri="mongodb://arl:ARL3141@db:27017/arl"
 docker-compose down
