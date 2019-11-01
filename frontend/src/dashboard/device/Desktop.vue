7135743ced3e53a49b2f2b327a2bffccf61d7fb0
นายทัสนัย ศรีสวัสดิ์ <62070074@kmitl.ac.th>
2019-11-01 14:28:14 +0700

    Add DOCSTRING

--- frontend/src/dashboard/device/Desktop.vue
+++ frontend/src/dashboard/device/Desktop.vue
@@ -1,8 +1,32 @@
 <template>
+    <!--
+        !! HTML Here !!
+        เขียนใน <div></div> ที่เตรียมไว้ให้เท่านั้น
+     -->
     <div>
         
     </div>
 </template>
-<style scoped>
+<script>
+export default {
 
+}
+</script>
+<style scoped>
+    /*
+        !! CSS Here !!
+        พยายามใช้ Bootstrap / Bootstrap Vue
+        ถ้าต้องการเพิ่ม style ระวังเรื่องชื่อ class ด้วย
+        แนะนำให้ใช้ id
+        ตัวอย่างเช่น ...
+
+        // # คือ id
+        #app-header {
+            background-color: #333;
+        }
+        // . คือ class
+        .app-header {
+            background-color: #333;
+        }
+     */
 </style>
