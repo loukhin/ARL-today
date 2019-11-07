a0a78dbd03c62d660f01a1b47d1ca4eee3b0524e
LoukHin <hatsuwara3@gmail.com>
2019-11-07 23:35:23 +0700

    Update database Schema

--- backend/models/timetable.js
+++ backend/models/timetable.js
@@ -2,17 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const timeTableSchema = new Schema({
-    _id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        auto: true
-    },
-    isExpress: Boolean,
     originEN: String,
     destinationEN: String,
     originTH: String,
     destinationTH: String,
-    time: {
+    timeNormal: {
+        type: Map,
+        of: Array
+    },
+    timeHoliday: {
         type: Map,
         of: Array
     }
