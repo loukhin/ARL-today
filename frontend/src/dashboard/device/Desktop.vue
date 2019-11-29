ca4f6994be3f4774f76ee1acf10ef4d9bc532478
SilverSky9 <nopphadon.phanwong@gmail.com>
2019-11-29 12:28:17 +0700

    Prevent selecting  duplicate station on Desktop

--- frontend/src/dashboard/device/Desktop.vue
+++ frontend/src/dashboard/device/Desktop.vue
@@ -114,6 +114,7 @@ export default {
     doEstimate() {
       let fromIndex = this.station.from.value;
       let toIndex = this.station.to.value;
+      if (fromIndex === toIndex) return;
       if (toIndex > fromIndex) {
         this.trainTo = 8;
         this.nextStation = fromIndex + 1;
