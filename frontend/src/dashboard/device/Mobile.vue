aac787b9532e921f1393ac494587d0a4f2c0aee6
นายทัสนัย ศรีสวัสดิ์ <62070074@kmitl.ac.th>
2019-12-01 11:34:06 +0700

    Connect Time selector to api

--- frontend/src/dashboard/device/Mobile.vue
+++ frontend/src/dashboard/device/Mobile.vue
@@ -132,12 +132,24 @@ export default {
         this.trainTo = 1;
         this.nextStation = fromIndex - 1;
       }
+      let specificTime = this.selectedTime;
       let reqAPI =
-        "https://app.loukhin.com/arl-today/api/get/" + fromIndex + "/" + toIndex;
-      axios.get(reqAPI).then(res => {
-        this.schedules = res.data.time.length !== 0 ? res.data.time : ["--:--"];
-        this.price = res.data.price;
-      });
+        "https://app.loukhin.com/arl-today/api/get/" + fromIndex + "/" + toIndex
+      if (specificTime === "Current Time") {
+        let reqAPI =
+        "https://app.loukhin.com/arl-today/api/get/" + fromIndex + "/" + toIndex
+        axios.get(reqAPI).then(res => {
+          this.schedules = res.data.time.length !== 0 ? res.data.time : ["--:--"];
+          this.price = res.data.price;
+        });
+      } else {
+        let reqAPI =
+        "https://app.loukhin.com/arl-today/api/get/" + fromIndex + "/" + toIndex + "/" + specificTime;
+        axios.get(reqAPI).then(res => {
+          this.schedules = res.data.time.length !== 0 ? res.data.time : ["--:--"];
+          this.price = res.data.price;
+        });
+      }
     },
     timeRemaining(time) {
       if (time === "--:--") return;
