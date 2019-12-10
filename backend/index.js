acd0f6036980c6b29eecdb764fbc1d6a63d9c20f
nirachastitch <nirachastitch@gmail.com>
2019-12-10 16:34:29 +0700

    Update helper path

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-const { sleep } = require('./helpers')
+const { sleep } = require('./routes/helpers')
 
 const port = 80
 
