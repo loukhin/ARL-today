07a15cf9831ba980967efe3dc8633534a16cdc6c
นายทัสนัย ศรีสวัสดิ์ <62070074@kmitl.ac.th>
2019-12-10 20:32:43 +0700

    Update doEstimate function

--- frontend/src/dashboard/device/Desktop.vue
+++ frontend/src/dashboard/device/Desktop.vue
@@ -150,8 +150,14 @@ export default {
         this.trainTo = 1
         this.nextStation = fromIndex - 1
       }
-      let reqAPI =
-        "https://app.loukhin.com/arl-today/api/get/" + fromIndex + "/" + toIndex
+      let dayType = (this.time.isHoliday) ? 'holiday' : 'weekday'
+      let specificTime = this.time.selected
+      let reqAPI
+      if (specificTime === "Current Time") {
+        reqAPI = `https://app.loukhin.com/arl-today/api/get/${fromIndex}/${toIndex}/now`
+      } else {
+        reqAPI = `https://app.loukhin.com/arl-today/api/get/${fromIndex}/${toIndex}/${specificTime}/${dayType}`
+      }
       axios.get(reqAPI).then(res => {
         this.schedules = res.data.time.length !== 0 ? res.data.time : ["--:--"]
         this.price = res.data.price
