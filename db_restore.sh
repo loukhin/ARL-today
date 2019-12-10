262521cedd91c7302a5360a9ec71a240386e0df1
Nutchayaporn <earnsiangkhio@hotmail.co.th>
2019-12-10 18:51:50 +0700

    Remove scripts

--- db_restore.sh
+++
@@ -1,2 +0,0 @@
-echo "Restoring data from mongodb/dump/arl"
-docker-compose exec db mongorestore --uri="mongodb://arl:ARL3141@db:27017/arl" /data/db/dump/
--- stop.sh
+++
@@ -1,3 +0,0 @@
-echo "Backing up Database to mongodb/dump/"
-docker-compose exec db mongodump -o /data/db/dump/ --uri="mongodb://arl:ARL3141@db:27017/arl"
-docker-compose down
